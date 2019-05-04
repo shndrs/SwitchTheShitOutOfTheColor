a7662ced5663dcbd4b7805307f39e05e92045f08
NP2 <home@NP2s-iMac-2.local>
2019-05-04 17:03:33 +0430

    Delete Some assets and clean some of the codes

--- SwitchTheShitOutOfTheColor/Scenes/GameScene.swift
+++ SwitchTheShitOutOfTheColor/Scenes/GameScene.swift
@@ -8,19 +8,6 @@
 
 import SpriteKit
 
-enum PlayColors {
-    static let colors = [
-        UIColor(red: 231/255, green: 76/255, blue: 60/255, alpha: 1.0),
-        UIColor(red: 241/255, green: 196/255, blue: 15/255, alpha: 1.0),
-        UIColor(red: 46/255, green: 204/255, blue: 113/255, alpha: 1.0),
-        UIColor(red: 52/255, green: 152/255, blue: 219/255, alpha: 1.0)
-    ]
-}
-
-enum SwitchState: Int {
-    case red, yellow, green, blue
-}
-
 final class GameScene: SKScene {
     
     private var colorSwitch: SKSpriteNode!
