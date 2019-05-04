d2c87f792506bd8f7198ea04de42068fe60071ab
NP2 <home@NP2s-iMac-2.local>
2019-05-04 10:19:44 +0430

    Initial Commit

---
+++ SwitchTheShitOutOfTheColor/GameScene.swift
@@ -0,0 +1,17 @@
+//
+//  GameScene.swift
+//  SwitchTheShitOutOfTheColor
+//
+//  Created by NP2 on 5/4/19.
+//  Copyright © 2019 shndrs. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+}
---
+++ SwitchTheShitOutOfTheColor/GameViewController.swift
@@ -0,0 +1,32 @@
+//
+//  GameViewController.swift
+//  SwitchTheShitOutOfTheColor
+//
+//  Created by NP2 on 5/4/19.
+//  Copyright © 2019 shndrs. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+            
+            let scene = GameScene(size: view.bounds.size)
+            
+            scene.scaleMode = .aspectFill
+            
+            view.presentScene(scene)
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+
+}
